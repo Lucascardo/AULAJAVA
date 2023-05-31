3506cd6c5b7cf740d8f7a43de904e5a720003040
Lucascardo <Lucassouzacardoso@5gmail.com>
2023-05-30 21:11:47 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/primeiroprojeto/Primeiroprojeto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiroprojeto;
+
+/**
+ *
+ * @author Cliente
+ */
+public class Primeiroprojeto {
+    
+    public int num1;
+    public int num2;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
